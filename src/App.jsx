af664fb64abcaaeec94f36b384c0ff7c66a8c43d
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-02 05:15:33 +0600

    added header and installed tainwind and daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Foods from "./components/Foods/Foods";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App max-w-7xl mx-auto">
+      <Header></Header>
       <Foods></Foods>
     </div>
   );
